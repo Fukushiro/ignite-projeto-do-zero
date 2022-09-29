ebc96cff7532b88498d2e833cde66d2173b09622
DESKTOP-ELGSLMU\Fukushiro <jpflc2301@gmail.com>
2022-09-29 16:40:06 -0300

    feat : first commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <div className={styles.container}>
+      <Image src="/images/icon.svg" width={50} height={50} />
+
+      <h2>spacetraveling</h2>
+    </div>
+  );
 }
