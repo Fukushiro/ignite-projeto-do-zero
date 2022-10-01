d425721a94de1e64099324a4f1c784c234f4c0e2
DESKTOP-ELGSLMU\Fukushiro <jpflc2301@gmail.com>
2022-10-01 16:32:25 -0300

    fix : made header clickable

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import styles from './header.module.scss';
-
+import Link from 'next/link';
 export default function Header() {
   return (
     <div className={styles.container}>
-      <Image src="/images/Logo.svg" width={238} height={50} />
-
+      <Link href={'/'}>
+        <a href="">
+          <Image src="/images/Logo.svg" width={238} height={50} alt="logo" />
+        </a>
+      </Link>
       {/* <h2>spacetraveling</h2> */}
     </div>
   );
